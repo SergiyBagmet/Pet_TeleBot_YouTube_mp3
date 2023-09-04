cf0bad36533a11e7ef554e517135096a3dc3d771
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-09-04 20:43:46 +0300

    error handler

--- package/telegram_bot/telebot.py
+++ package/telegram_bot/telebot.py
@@ -1,5 +1,5 @@
 from .API import TOKEN_API
-from ..conversion.convert import ConvertYouTube
+from ..conversion.convert import ConvertYouTube, ConvertErrorYouTube
 
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, CallbackContext, Filters
@@ -11,24 +11,29 @@ def start(update: Update, context: CallbackContext) :
         chat_id = update.effective_chat.id,
         text="Hi send me a link to music from youtube."
         )
-  
-def download_mp3(update: Update, context: CallbackContext) -> None:
-    link = update.message.text
+
+def error_handler(update: Update, context: CallbackContext):
     try:
-        yt = ConvertYouTube(link)
+        raise context.error
+    except ConvertErrorYouTube:
         context.bot.send_message(
         chat_id = update.effective_chat.id,
-        text="Please wait loading."
+        text=f"Error {context.error} "
         )
-        yt.link_to_mp3_transaction()
-        context.bot.send_audio(
-            chat_id=update.message.chat_id, 
-            audio = open(yt.path_mp3, "rb")
-            )
-    except ValueError:
-        update.message.reply_text("Error.")
     
-    
+def download_mp3(update: Update, context: CallbackContext) -> None:
+    link = update.message.text
+    ConvertYouTube.valid_one_video_link(link)
+    yt = ConvertYouTube(link)
+    context.bot.send_message(
+    chat_id = update.effective_chat.id,
+    text="Please wait loading."
+    )
+    yt.link_to_mp3_transaction()
+    context.bot.send_audio(
+        chat_id=update.message.chat_id, 
+        audio = open(yt.path_mp3, "rb")
+        )
 
 
 def run_telebot():
@@ -39,8 +44,9 @@ def run_telebot():
     # Создайте обработчики команды /start и текстовых сообщений
     start_handler = CommandHandler('start', start)
     downlod_mp3_handler = MessageHandler(Filters.text & ~Filters.command, download_mp3)
-
+    
     dispatcher.add_handler(start_handler)
+    dispatcher.add_error_handler(error_handler)
     dispatcher.add_handler(downlod_mp3_handler)
 
     # Запустите бота
